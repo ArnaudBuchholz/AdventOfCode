87d91d6f3c7fe616f8a7910200a436f1ddb76ba0
ArnaudBuchholz <arnaud.buchholz@free.fr>
2021-12-04 15:53:49 -0500

    Simplify input

--- input.js
+++ input.js
@@ -1,13 +1,16 @@
 const { accessSync: access, readFileSync: readFile } = require('fs')
-const { join } = require('path')
+const { join, relative } = require('path')
 
-module.exports = function (day) {
-  const fileName = join(__dirname, `input/${day}.txt`)
-  try {
-    access(fileName)
-    return readFile(fileName).toString()
-  } catch (e) {
-    console.error(`Missing input for day #${day}`)
-    process.exit(-1)
-  }
+const [, script, additional] = process.argv
+const day = relative(__dirname, script)
+const folder = additional === '-sample' ? 'sample' : 'input'
+
+const fileName = join(__dirname, folder, `${day}.txt`)
+
+try {
+  access(fileName)
+  module.exports = readFile(fileName).toString()
+} catch (e) {
+  console.error(`Missing ${folder} for day #${day}`)
+  process.exit(-1)
 }
