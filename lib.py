b6c16789b1b8c89f3b0daeb5c8f6f27a76e04322
ArnaudBuchholz <arnaud.buchholz@free.fr>
2021-12-14 18:20:54 -0500

    Linter fixes

--- lib.py
+++ lib.py
@@ -1,15 +1,27 @@
-import sys, re, math
+import sys
+import re
+import math
 from pathlib import Path
 
 year_day_parser = re.compile(r'(\d+)(?:\\|/)(\d+)')
 year, day = year_day_parser.search(sys.argv[1]).groups()
 
-input_path = Path.cwd() / year / 'input' / (day + '.txt')
+if (len(sys.argv) >= 3 and sys.argv[2] == '-sample'):
+  folder = 'sample'
+else:
+  folder = 'input'
+
+input_path = Path.cwd() / year / folder / (day + '.txt')
 script_path = Path.cwd() / year / (day + '.py')
 
 with open(input_path) as input:
   lines = input.readlines()
 
-numbers = [*map(int, lines)]
+lines = [*map(lambda s: s.strip(), lines)]
+
+try:
+  numbers = [*map(int, lines)]
+except ValueError:
+  numbers = []
 
 exec(open(script_path).read())
