f9ae540dbd7a7d876078a084c286d8442328f007
ArnaudBuchholz <arnaud.buchholz@free.fr>
2021-12-14 17:12:11 -0500

    Day 1

---
+++ 2019/1.py
@@ -0,0 +1,16 @@
+fuel_per_mass = lambda x : max(math.floor(x / 3) - 2, 0)
+
+fuel_per_module = [*map(fuel_per_mass, numbers)]
+
+fuel_for_all_modules = sum(fuel_per_module)
+print('Part 1 :', fuel_for_all_modules)
+
+def fuel_per_fuel (mass):
+  if (mass == 0):
+    return 0
+  else:
+    fuel = fuel_per_mass(mass)
+    return fuel + fuel_per_fuel(fuel)
+
+fuel_for_modules_fuel = [*map(fuel_per_fuel, fuel_per_module)]
+print('Part 2 :', fuel_for_all_modules + sum(fuel_for_modules_fuel))
