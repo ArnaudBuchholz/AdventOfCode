b7825ec6b8f3432fc47822f124ac3144b202bb49
ArnaudBuchholz <arnaud.buchholz@free.fr>
2021-12-15 10:59:27 -0500

    Removes useless int

--- lib.js
+++ lib.js
@@ -7,18 +7,20 @@ const folder = additional && additional.startsWith('-sample') ? 'sample' : 'inpu
 const sampleIndex = (/-sample(\d)/.exec(additional) || [])[1]
 const fileName = join(__dirname, year, folder, `${day}${sampleIndex ? '.' + sampleIndex : ''}.txt`)
 
-const int = n => parseInt(n, 10)
-
 try {
   access(fileName)
   const input = readFile(fileName).toString()
   const lines = input.split(/\r?\n/).filter(line => !!line.trim())
-  const numbers = lines.map(int)
+  let numbers
+  try {
+    numbers = lines.map(Number)
+  } catch (e) {
+    numbers = []
+  }
   module.exports = {
     input,
     lines,
-    numbers,
-    int
+    numbers
   }
 } catch (e) {
   console.error(`Missing ${folder} for day #${day}`)
