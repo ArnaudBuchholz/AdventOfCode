b46c0214b59d6c381d4cd9ed1b049e1fc2caf807
ArnaudBuchholz <arnaud.buchholz@free.fr>
2022-12-18 09:14:42 -0500

    Rename APIs and provide a range building helper

--- 2022/4.js
+++ 2022/4.js
@@ -1,7 +1,7 @@
 require('../challenge')(async function * ({
   lines
 }) {
-  const { contains, overlaps } = await require('../lib/range')
+  const { contain, overlap } = await require('../lib/range')
 
   const pairs = lines.map(line => {
     const [, rawFrom1, rawTo1, rawFrom2, rawTo2] = line.match(/(\d+)-(\d+),(\d+)-(\d+)/)
@@ -13,6 +13,6 @@ require('../challenge')(async function * ({
 
   console.log(pairs)
 
-  yield pairs.reduce((total, [r1, r2]) => contains(r1, r2) ? total + 1 : total, 0)
-  yield pairs.reduce((total, [r1, r2]) => overlaps(r1, r2) ? total + 1 : total, 0)
+  yield pairs.reduce((total, [r1, r2]) => contain(r1, r2) ? total + 1 : total, 0)
+  yield pairs.reduce((total, [r1, r2]) => overlap(r1, r2) ? total + 1 : total, 0)
 })
