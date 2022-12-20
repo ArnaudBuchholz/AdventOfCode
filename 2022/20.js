39e60c98da5c14ad8ce0b26bf36dd5aba4b88998
ArnaudBuchholz <arnaud.buchholz@free.fr>
2022-12-20 15:43:41 -0500

    2022/20 (not working)

---
+++ 2022/20.js
@@ -0,0 +1,74 @@
+require('../challenge')(async function * ({
+  assert,
+  isSample,
+  numbers
+}) {
+  console.log(numbers.join(','), numbers.length)
+
+  function mix (encrypted, number) {
+    const startingPos = encrypted.indexOf(number)
+    assert.ok(startingPos !== -1)
+    const { length } = encrypted
+    let finalPos = startingPos
+    if (number > 0) {
+      finalPos += number
+      while (finalPos >= length) {
+        finalPos -= length - 1
+      }
+    } else if (number < 0) {
+      finalPos += number
+      while (finalPos <= 0) {
+        finalPos += length - 1
+      }
+    }
+    const decrypted = [...encrypted]
+    if (startingPos < finalPos) {
+      decrypted.splice(finalPos + 1, 0, number)
+      decrypted.splice(startingPos, 1)
+    } else {
+      decrypted.splice(finalPos, 0, number)
+      decrypted.splice(startingPos + 1, 1)
+    }
+    return decrypted
+  }
+
+  const check = (array, number, expected) => assert.strictEqual(mix(array, number).join(', '), expected.join(', '), `check([${array.join(', ')}], ${number}, [${expected.join(', ')}])`)
+
+  check([1, 2, 3], 1, [2, 1, 3])
+  check([2, 1, 3], 1, [2, 3, 1])
+  check([2, 3, 1], 1, [2, 1, 3])
+
+  check([1, 2, 3, 4], 1, [2, 1, 3, 4])
+  check([2, 1, 3, 4], 1, [2, 3, 1, 4])
+  check([2, 3, 1, 4], 1, [2, 3, 4, 1])
+  check([2, 3, 4, 1], 1, [2, 1, 3, 4])
+
+  check([-1, 2, 3], -1, [2, -1, 3])
+  check([2, -1, 3], -1, [2, 3, -1])
+  check([2, 3, -1], -1, [2, -1, 3])
+
+  check([1, 2, -3, 3, -2, 0, 4], 1, [2, 1, -3, 3, -2, 0, 4])
+  check([2, 1, -3, 3, -2, 0, 4], 2, [1, -3, 2, 3, -2, 0, 4])
+  check([1, -3, 2, 3, -2, 0, 4], -3, [1, 2, 3, -2, -3, 0, 4])
+  check([1, 2, 3, -2, -3, 0, 4], 3, [1, 2, -2, -3, 0, 3, 4])
+  check([1, 2, -2, -3, 0, 3, 4], -2, [1, 2, -3, 0, 3, 4, -2])
+  check([1, 2, -3, 0, 3, 4, -2], 0, [1, 2, -3, 0, 3, 4, -2])
+  check([1, 2, -3, 0, 3, 4, -2], 4, [1, 2, -3, 4, 0, 3, -2])
+
+  let decrypted = [...numbers]
+  numbers.forEach((number) => {
+    decrypted = mix(decrypted, number)
+  })
+
+  if (isSample) {
+    assert.strictEqual(decrypted.join(', '), '1, 2, -3, 4, 0, 3, -2')
+  }
+
+  const posOf0 = decrypted.indexOf(0)
+  const first = decrypted[(posOf0 + 1000) % decrypted.length]
+  const second = decrypted[(posOf0 + 2000) % decrypted.length]
+  const third = decrypted[(posOf0 + 3000) % decrypted.length]
+  console.log('1000th :', first, '\n2000th :', second, '\n3000th :', third)
+
+  yield first + second + third // > 4060
+})
