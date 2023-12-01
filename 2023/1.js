faf11dde9023f96c452c132b57bac2d2231734e8
ArnaudBuchholz <arnaud.buchholz@free.fr>
2023-12-01 09:22:21 -0500

    feat(2023/1): ok

---
+++ 2023/1.js
@@ -0,0 +1,40 @@
+require('../challenge')(function * ({
+  lines,
+  verbose
+}) {
+  yield lines.reduce((total, line) => {
+    const firstDigit = line.match(/^[a-z]*([0-9])/i)[1]
+    const lastDigit = line.match(/([0-9])[a-z]*$/i)[1]
+    if (verbose) {
+      console.log(line, firstDigit, lastDigit)
+    }
+    const value = parseInt(firstDigit + lastDigit, 10)
+    return total + value
+  }, 0)
+
+  yield lines.reduce((total, line) => {
+    let substituted = line
+    // We can't just find and replace in any order (example: eightwothree => 823),
+    // we need to scan the line sequentially to check if it contains a spelled number.
+    // This is *not* the most efficient way...
+    for (let pos = 0; pos < substituted.length; ++pos) {
+      const stringAtPos = substituted.substring(pos)
+      let number = 1
+      for (let spelling of ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']) {
+        if (stringAtPos.startsWith(spelling)) {
+          substituted = substituted.substring(0, pos) + number + substituted.substring(pos)
+          pos++ // skip injected digit
+          break
+        }
+        ++number
+      }
+    }
+    const firstDigit = substituted.match(/^[a-z]*([0-9])/i)[1]
+    const lastDigit = substituted.match(/([0-9])[a-z]*$/i)[1]
+    if (verbose) {
+      console.log(line, substituted, firstDigit, lastDigit)
+    }
+    const value = parseInt(firstDigit + lastDigit, 10)
+    return total + value
+  }, 0)
+})
--- README.md
+++ README.md
@@ -6,6 +6,7 @@ You can access some of the challenges from the browser :
 * Open a browser to http://localhost:8080/
 
 They can be run using command line :
+* [2023](https://adventofcode.com/2023) made with Node.js, run `node 2023/<day> [-sample]?`
 * [2022](https://adventofcode.com/2022) made with Node.js, run `node 2022/<day> [-sample]?`
 * [2021](https://adventofcode.com/2021) made with Node.js, run `node 2021/<day> [-sample]?`
 * [2019](https://adventofcode.com/2019) made with Python, run `python 2019/<day> [-sample]?`
