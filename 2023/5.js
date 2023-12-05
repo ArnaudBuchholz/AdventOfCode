28308c77d5c1b2592f39cb67e50a99ce3044fb20
ArnaudBuchholz <arnaud.buchholz@free.fr>
2023-12-05 07:07:49 -0500

    feat(2023/5): ok

--- 2023/5.js
+++ 2023/5.js
@@ -1,7 +1,9 @@
-require('../challenge')(function * ({
+require('../challenge')(async function * ({
   lines,
   verbose
 }) {
+  const { getContained, getIntersection } = await require('../lib/range')
+
   const seeds = lines[0].split(':')[1].trim().split(' ').map(Number)
 
   const mappings = []
@@ -25,34 +27,100 @@ require('../challenge')(function * ({
     console.log('mappings:', JSON.stringify(mappings, undefined, 2))
   }
 
-  function getLocation (seed) {
-    let value = seed
-    mappings.forEach(({ title, ranges }) => ranges.every(({ destination, source, length }) => {
-      const offset = value - source
-      if (offset >= 0 && offset < length) {
-        value = destination + offset
-        return false
+  function getMinLocation (base, count) {
+    let minLocation = Number.POSITIVE_INFINITY
+    const stack = [{
+      seed: base, // to keep track
+      from: base,
+      to: base + count - 1,
+      mapping: 0,
+      range: 0
+    }]
+    while (stack.length) {
+      const { seed, from, to, mapping, range } = stack.pop()
+      if (mapping >= mappings.length) {
+        // Went over mappings
+        minLocation = Math.min(minLocation, from)
+        continue
       }
-      return true
-    }))
-    return value
+      const mappingDef = mappings[mapping]
+      const { title } = mappingDef
+      if (range >= mappingDef.ranges.length) {
+        // Went over mappings' ranges
+        if (verbose) {
+          console.log(`Seed ${seed} ${title} ${seed}`)
+        }
+        stack.push({
+          seed,
+          from,
+          to,
+          mapping: mapping + 1,
+          range: 0
+        })
+        continue
+      }
+      const { destination, source, length } = mappingDef.ranges[range]
+      const intersect = getIntersection([from, to], [source, source + length - 1])
+      if (intersect === null) {
+          stack.push({
+          seed,
+          from,
+          to,
+          mapping,
+          range: range + 1
+        })
+        continue
+      }
+      // Intersection goes to next mapping
+      const newFrom = intersect[0] - source + destination
+      if (verbose) {
+        console.log(`Seed ${seed} ${title} ${newFrom}`)
+      }
+      stack.push({
+        seed,
+        from: newFrom,
+        to: intersect[1] - source + destination,
+        mapping: mapping + 1,
+        range: 0
+      })
+      // Anything left goes to next range
+      if (intersect[0] > from) {
+        stack.push({
+          seed,
+          from,
+          to: intersect[0] - 1,
+          mapping: mapping,
+          range: range + 1
+        })
+      }
+      if (intersect[1] < to) {
+        stack.push({
+          seed,
+          from: intersect[1] + 1,
+          to,
+          mapping: mapping,
+          range: range + 1
+        })
+      }
+    }
+    return minLocation
   }
 
-  const locations = seeds.map(getLocation)
+  const locations = seeds.map(seed => getMinLocation(seed, 1))
+  if (verbose) {
+    console.log('Step 1 locations:', locations)
+  }
   yield Math.min(...locations)
 
-  // Brut force
   const seedRanges = [...seeds]
   let minLocation = Number.POSITIVE_INFINITY
   while (seedRanges.length) {
     const base = seedRanges.shift()
     const count = seedRanges.shift()
     if (verbose) {
-      console.log(`Processing [${base}; ${base + count - 1}]...`)
+      console.log(`Processing ${base}...`)
     }
-    for (let index = 0; index < count; ++index) {
-      minLocation = Math.min(minLocation, getLocation(base + index))
-    }
+    minLocation = Math.min(minLocation, getMinLocation(base, count))
   }
   yield minLocation
 })
