67b6f779b98c144f88f3e5df2f9d8931bb78b21b
ArnaudBuchholz <arnaud.buchholz@free.fr>
2023-12-11 07:54:13 -0500

    feat(2023/11): part 2 ok

--- 2023/11.js
+++ 2023/11.js
@@ -1,71 +1,71 @@
 require('../challenge')(async function * ({
+  isSample,
   lines,
   verbose
 }) {
-  const usedColumns = []
-  lines.forEach(line => {
-    line.replace(/#/g, (match, offset) => {
-      if (!usedColumns.includes(offset)) {
-        usedColumns.push(offset)
-      }
-      return '#'
-    })
-  })
-  usedColumns.sort()
-  if (verbose) {
-    console.log('Detected columns to be used :', usedColumns)
-  }
-
-  // expand
-  const expanded = lines.map(line => {
-    const chars = line.split('').map((char, index) => {
-      if (usedColumns.includes(index)) {
-        return char
-      }
-      if (char !== '.') {
-        throw new Error('Should not be !')
-      }
-      return [char, char]
-    }).flat().join('')
-
-    if (!line.includes('#')) {
-      return [chars, chars]
-    }
-    return chars
-  }).flat()
-  if (verbose) {
-    console.log('Expanded universe :\n' + expanded.join('\n'))
-  }
-
-  // Another approach could be to value some lines & columns 2 instead of 1
-
+  const usedXs = []
+  const usedYs = []
   const galaxies = []
-  expanded.forEach((line, y) => {
+  lines.forEach((line, y) => {
     line.replace(/#/g, (match, x) => {
+      if (!usedYs.includes(y)) {
+        usedYs.push(y)
+      }
+      if (!usedXs.includes(x)) {
+        usedXs.push(x)
+      }
       galaxies.push({ x, y })
       return '#'
     })
   })
   if (verbose) {
-    console.log('Galaxies :', galaxies)
+    console.log('Detection :')
+    console.log('\tGalaxies :', galaxies)
+    console.log('\tColumns :', usedXs)
+    console.log('\tLines :', usedYs)
   }
 
-  function distance (g1, g2) {
-    return Math.abs(g2.x - g1.x) + Math.abs(g2.y - g1.y)
-  }
-
-  let sum = 0
-  for (let i = 0; i < galaxies.length; ++i) {
-    for (let j = i + 1; j < galaxies.length; ++j) {
-      const distanceBetween = distance(galaxies[i], galaxies[j])
-      sum += distanceBetween
-      if (verbose) {
-        console.log(i, '⟺ ', j, ' = ', distanceBetween)
+  function distance (g1, g2, weightOfUnused) {
+    let result = -2
+    const startX = Math.min(g1.x, g2.x)
+    const endX = Math.max(g1.x, g2.x)
+    for (let x = startX; x <= endX; ++x) {
+      if (usedXs.includes(x)) {
+        ++result
+      } else {
+        result += weightOfUnused
       }
     }
+    const startY = Math.min(g1.y, g2.y)
+    const endY = Math.max(g1.y, g2.y)
+    for (let y = startY; y <= endY; ++y) {
+      if (usedYs.includes(y)) {
+        ++result
+      } else {
+        result += weightOfUnused
+      }
+    }
+    return result
   }
 
-  yield sum
+  function distanceBetweenGalaxies (weightOfUnused) {
+    let sum = 0
+    for (let i = 0; i < galaxies.length; ++i) {
+      for (let j = i + 1; j < galaxies.length; ++j) {
+        const distanceBetween = distance(galaxies[i], galaxies[j], weightOfUnused)
+        sum += distanceBetween
+        if (verbose) {
+          console.log(i, '⟺ ', j, ' = ', distanceBetween)
+        }
+      }
+    }
+    return sum
+  }
 
+  yield distanceBetweenGalaxies(2)
+  if (isSample) {
+    console.log(10, distanceBetweenGalaxies(10))
+    console.log(100, distanceBetweenGalaxies(100))
+  }
+  yield distanceBetweenGalaxies(1000000)
 })
-  
