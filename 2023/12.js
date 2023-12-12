20c9c8986f2945db51997aae89d1061b50a00554
ArnaudBuchholz <arnaud.buchholz@free.fr>
2023-12-12 09:13:56 -0500

    feat(2023/12): attempt to make it more efficient

--- 2023/12.js
+++ 2023/12.js
@@ -3,46 +3,106 @@ require('../challenge')(function * ({
   verbose
 }) {
   function getNumberOfArrangements (line) {
-    const [pattern, validation] = line.split(' ')
-    const consecutiveGroups = validation.split(',').map(Number)
-    const numberOfUnknowns = pattern.replace(/[^?]/g, '').length
-    const numberOfAttempts = Math.pow(2, numberOfUnknowns)
-    if (verbose) {
-      console.log({
-        pattern,
-        consecutiveGroups,
-        numberOfUnknowns,
-        numberOfAttempts
-      })
-    }
+    const parts = line.split(' ')
+    const stack = [{
+      processed: '',
+      pattern: parts[0].split(''),
+      groups: parts[1].split(',').map(Number),
+      expected: ''
+    }]
     let arrangements = 0
-    for (let attempt = 0; attempt < numberOfAttempts; ++attempt) {
-      let bit = 1
-      const resolved = pattern.replace(/\?/g, () => {
-        const result = (attempt & bit) === bit ? '#' : '.'
-        bit *= 2
-        return result
-      })
-      const groups = []
-      resolved.replace(/(#+)/g, match => groups.push(match.length))
-      let ok = false
-      if (groups.length === consecutiveGroups.length && groups.every((count, index) => consecutiveGroups[index] === count)) {
+    let iterations = 0
+
+    while (stack.length) {
+      ++iterations
+      const {
+        processed,
+        pattern: [current, ...pattern],
+        groups,
+        expected
+      } = stack.pop()
+
+      // Exit conditions
+      if (current === undefined && expected !== '#' && groups.length === 0) {
         ++arrangements
-        ok = true
+        continue // YES
       }
-      if (verbose) {
-        console.log(attempt, resolved, groups, ok)
+      if (current === undefined ||
+        (expected === '.' && current === '#') ||
+        (expected === '#' && current === '.') ||
+        (groups.length === 0 && (current === '#' || pattern.includes('#')))
+      ) {
+        continue // NOPE
+      }
+
+      const checkGroup = () => {
+        if (current === '.') {
+          return // NOPE
+        }
+        // Otherwise we have # or ?
+        const [left, ...otherGroups] = groups
+        if (left === 1) {
+          stack.push({
+            processed: processed + '#', // current,
+            pattern,
+            groups: otherGroups,
+            expected: '.'
+          })
+        } else {
+          stack.push({
+            processed: processed + '#', // current,
+            pattern,
+            groups: [left - 1, ...otherGroups],
+            expected: '#'
+          })
+        }
+      }
+
+      const next = () => stack.push({
+        processed: processed + '.', // current
+        pattern,
+        groups,
+        expected: ''
+      })
+
+      if (expected === '.') {
+        next()
+      } else if (expected === '#') {
+        checkGroup()
+      } else { // expected = ''
+        if (current === '.') {
+          next()
+        } else if (current === '#') {
+          checkGroup()
+        } else if (current === '?') {
+          next()
+          checkGroup()
+        }
       }
     }
+
     if (verbose) {
-      console.log({ arrangements })
+      console.log(line, '➔', arrangements, '(', iterations, ')')
     }
     return arrangements
   }
 
   // getNumberOfArrangements('???.### 1,1,3')
   // getNumberOfArrangements('.??..??...?##. 1,1,3 ')
+  // getNumberOfArrangements('????.######..#####. 1,6,5')
   // getNumberOfArrangements('?###???????? 3,2,1')
+  // getNumberOfArrangements('.???#?#????..????. 1,4,1,1,2,1')
 
   yield lines.reduce((total, line) => total + getNumberOfArrangements(line), 0)
+
+  const x5 = line => {
+    const [p, v] = line.split(' ')
+    return `${p}?${p}?${p}?${p}?${p} ${v},${v},${v},${v},${v}`
+  }
+
+  // getNumberOfArrangements(x5('???.### 1,1,3'))
+  // getNumberOfArrangements(x5('?###???????? 3,2,1'))
+  // getNumberOfArrangements(x5('.???#?#????..????. 1,4,1,1,2,1'))
+
+  // yield lines.reduce((total, line) => total + getNumberOfArrangements(x5(line)), 0)
 })
