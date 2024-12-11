50455d7860b556ea533db66399094e3047ffa9eb
ArnaudBuchholz <arnaud.buchholz@free.fr>
2024-12-11 08:16:43 -0500

    feat(2024/11): part2 ok

--- 2024/11.js
+++ 2024/11.js
@@ -6,8 +6,9 @@ require('../challenge')(async function * ({
 
   const stones = lines[0].split(' ').map(Number)
 
-  function iterate (numberOfSteps) {
-    const loop = buildLoopControl(Number.POSITIVE_INFINITY)
+  const VALIDATE = false
+
+  function oldIterate (numberOfSteps) {
     const state = [...stones]
     if (verbose) {
       console.log('Initial :', state.join(','))
@@ -15,7 +16,6 @@ require('../challenge')(async function * ({
     for (let step = 0; step < numberOfSteps; ++step) {
       let index = 0
       while (index < state.length) {
-        loop.log('Iterating... {step}: {count}', { step, count: state.length })
         const stone = state[index]
         if (stone === 0) {
           state[index] = 1
@@ -32,11 +32,59 @@ require('../challenge')(async function * ({
           ++index
         }
       }
+    }
+    return state
+  }
+
+  /** Since order is not significant, trying a different approach */
+  function iterate (numberOfSteps) {
+    const loop = buildLoopControl(Number.POSITIVE_INFINITY)
+    let state = new Map()
+    stones.forEach(stone => state.set(stone, (state.get(stone) ?? 0) + 1))
+    if (verbose) {
+      console.log('Initial :', state)
+    }
+    for (let step = 0; step < numberOfSteps; ++step) {
+      const newState = new Map()
+      const currentStones = state.keys()
+      for (const stone of currentStones) {
+        loop.log('Iterating... {step}: {count}', { step, count: state.length })
+        const count = state.get(stone)
+        if (stone === 0) {
+          newState.set(1, (newState.get(1) ?? 0) + count)
+        } else if (stone.toString().length % 2 === 0) {
+          const digits = stone.toString()
+          const length = digits.length / 2
+          const left = Number(digits.substring(0, length))
+          const right = Number(digits.substring(length))
+          newState.set(left, (newState.get(left) ?? 0) + count)
+          newState.set(right, (newState.get(right) ?? 0) + count)
+        } else {
+          const newStone = stone * 2024
+          newState.set(newStone, (newState.get(newStone) ?? 0) + count)
+        }
+      }
+      state = newState
       if (verbose) {
-        console.log(`[${step + 1}] :`, state.join(','))
+        let totalNumberOfStones = 0
+        state.forEach(count => { totalNumberOfStones += count })
+        console.log(`[${step + 1}] : ${totalNumberOfStones}`, state)
+
+        if (VALIDATE) {
+          const oldIterations = oldIterate(step + 1)
+          state.forEach((count, stone) => {
+            const oldCount = oldIterations.filter(value => value === stone).length
+            if (oldCount !== count) {
+              console.error(`Error on ${stone} : ${count} !== ${oldCount} (old)`)
+              process.exit(0)
+            }
+          })
+        }
       }
     }
-    return state.length
+    let totalNumberOfStones = 0
+    state.forEach(count => { totalNumberOfStones += count })
+    return totalNumberOfStones
   }
 
   yield iterate(25)
